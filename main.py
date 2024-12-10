8853969ebeb178fd1ed6855b65facc5f278f8528
koskirillal <koskirillal@gmail.com>
2024-12-10 22:45:42 +0300

    m

--- main.py
+++ main.py
@@ -114,34 +114,6 @@ def drop(message):
 
 
 
-@bot.message_handler(content_types=['photo'])
-def get_photo(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1 = (types.InlineKeyboardButton("Go to site" , url='https://olymp.mephi.ru/'))
-    markup.row(btn1)
-    btn2 = (types.InlineKeyboardButton("Delete photo" ,callback_data='delete'))
-    btn3 = (types.InlineKeyboardButton("Change text" ,callback_data='edit'))
-    markup.row(btn2 , btn3)
-    bot.reply_to(message , "Very beautifull" , reply_markup =markup)
-@bot.callback_query_handler(func = lambda callback: True)
-def callback_message(callback):
-    if(callback.data == 'delete'):
-        bot.delete_message(callback.message.chat.id , callback.message.message_id -1)
-    elif callback.data == 'edit':
-        bot.edit_message_text("Edit text" , callback.message.chat.id ,callback.message.message_id )
-
-
-
-
-
-@bot.message_handler(commands=['site' , 'mfti'])
-def site(message):
-    webbrowser.open("https://olymp-online.mipt.ru/")
-
-
-@bot.message_handler(commands=['mifi'])
-def site(message):
-    webbrowser.open("https://org.mephi.ru/index/index")
 
 @bot.message_handler(commands=['help'])
 def help(message):
